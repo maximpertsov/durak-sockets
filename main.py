437a01cc4d5e9ba4ac95acb5b929aeb0698d29ee
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-04 22:09:56 -0400

    allow requests from xchi.online

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from os import environ
 from broadcaster import Broadcast
 from fastapi import FastAPI, WebSocket
 from fastapi.concurrency import run_until_first_complete
+from fastapi.middleware.cors import CORSMiddleware
 
 broadcast = Broadcast(environ.get("REDISCLOUD_URL", "redis://localhost:6379"))
 app = FastAPI(on_startup=[broadcast.connect], on_shutdown=[broadcast.disconnect])
+
+origins = [
+    "http://localhost:3000",
+    "https://xchi.online",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.websocket("/ws/chat")
