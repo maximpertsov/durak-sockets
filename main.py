a6af532782bbd7cbc053fa7e0fd9368397b25780
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-08 20:15:45 -0400

    chatroom->room

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ app.add_middleware(
 )
 
 
-@app.websocket("/ws/chat")
-async def chatroom_ws(websocket: WebSocket):
+@app.websocket("/ws")
+async def room_ws(websocket: WebSocket):
     await websocket.accept()
     await run_until_first_complete(
-        (chatroom_ws_receiver, {"websocket": websocket}),
-        (chatroom_ws_sender, {"websocket": websocket}),
+        (room_ws_receiver, {"websocket": websocket}),
+        (room_ws_sender, {"websocket": websocket}),
     )
 
 
-async def chatroom_ws_receiver(websocket: WebSocket):
+async def room_ws_receiver(websocket: WebSocket):
     async for message in websocket.iter_text():
-        await broadcast.publish(channel="chatroom", message=message)
+        await broadcast.publish(channel="room", message=message)
 
 
-async def chatroom_ws_sender(websocket: WebSocket):
-    async with broadcast.subscribe(channel="chatroom") as subscriber:
+async def room_ws_sender(websocket: WebSocket):
+    async with broadcast.subscribe(channel="room") as subscriber:
         async for event in subscriber:
             await websocket.send_text(event.message)
