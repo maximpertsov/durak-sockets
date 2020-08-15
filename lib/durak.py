a4a93a85acf0edbd403752c157f0d62c56f0535b
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-15 00:17:42 -0400

    define attack action

---
+++ lib/durak.py
@@ -0,0 +1,55 @@
+class Card:
+    def __init__(self, *, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def serialize(self):
+        return {"rank": self.rank, "suit": self.suit}
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == self.suit
+
+
+class Hands:
+    def __init__(self, hands):
+        self._hands = {
+            player: [Card(**card) for card in cards] for player, cards in hands
+        }
+
+    def serialize(self):
+        return {
+            player: [card.serialize() for card in cards]
+            for player, cards in self._hands
+        }
+
+    def remove_card(self, *, player, card):
+        card_object = Card(**card)
+        self._hand.update(
+            player=[
+                _card for _card in self._player_hand(player) if _card == card_object
+            ]
+        )
+        return self
+
+    def _player_hand(self, player):
+        return self._hands[player]
+
+
+class Table:
+    def __init__(self, table):
+        self._table = [[Card(**card) for card in cards] for cards in table]
+
+    def serialize(self):
+        return [[card.serialize() for card in cards] for cards in self._table]
+
+    def add_card(self, *, card):
+        self._table.append([Card(**card)])
+        return self
+
+
+def attack(*, user, card, hands, table, **kwargs):
+    return {
+        **kwargs,
+        "hands": Hands(hands=hands).remove_card(player=user, card=card).serialize(),
+        "table": Table(table=table).add_card(card=card).serialize(),
+    }
