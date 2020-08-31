01d4c4ebd7cf4449e7ea28a68e1826721ab9bcad
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-30 20:48:09 -0400

    update game tests

--- tests/lib/durak/test_game.py
+++ tests/lib/durak/test_game.py
@@ -6,22 +6,8 @@ from lib.durak import Game
 @pytest.fixture
 def game():
     return Game.deserialize(
-        draw_pile=[
-            {"rank": "jack", "suit": "clubs"},
-            {"rank": "3", "suit": "spades"},
-            {"rank": "6", "suit": "clubs"},
-        ],
-        hands={
-            "anna": [
-                {"rank": "10", "suit": "diamonds"},
-                None,
-                {"rank": "10", "suit": "clubs"},
-                {"rank": "2", "suit": "spades"},
-                {"rank": "5", "suit": "clubs"},
-                {"rank": "8", "suit": "diamonds"},
-                {"rank": "2", "suit": "clubs"},
-            ],
-        },
+        draw_pile=["JC", "3S", "6C"],
+        hands={"anna": ["10D", None, "10C", "2S", "5C", "8D", "2C"]},
         pass_count=0,
         players=["anna"],
         table=[],
@@ -31,22 +17,8 @@ def game():
 
 def test_serialize(game):
     assert game.serialize() == {
-        "draw_pile": [
-            {"rank": "jack", "suit": "clubs"},
-            {"rank": "3", "suit": "spades"},
-            {"rank": "6", "suit": "clubs"},
-        ],
-        "hands": {
-            "anna": [
-                {"rank": "10", "suit": "diamonds"},
-                None,
-                {"rank": "10", "suit": "clubs"},
-                {"rank": "2", "suit": "spades"},
-                {"rank": "5", "suit": "clubs"},
-                {"rank": "8", "suit": "diamonds"},
-                {"rank": "2", "suit": "clubs"},
-            ]
-        },
+        "draw_pile": ["JC", "3S", "6C"],
+        "hands": {"anna": ["10D", None, "10C", "2S", "5C", "8D", "2C"]},
         "pass_count": 0,
         "players": ["anna"],
         "table": [],
@@ -55,59 +27,29 @@ def test_serialize(game):
 
 
 def test_attack(game):
-    game.attack(player="anna", card={"rank": "10", "suit": "diamonds"})
+    game.attack(player="anna", card="10D")
     assert game.serialize() == {
-        "draw_pile": [
-            {"rank": "jack", "suit": "clubs"},
-            {"rank": "3", "suit": "spades"},
-            {"rank": "6", "suit": "clubs"},
-        ],
-        "hands": {
-            "anna": [
-                None,
-                None,
-                {"rank": "10", "suit": "clubs"},
-                {"rank": "2", "suit": "spades"},
-                {"rank": "5", "suit": "clubs"},
-                {"rank": "8", "suit": "diamonds"},
-                {"rank": "2", "suit": "clubs"},
-            ]
-        },
+        "draw_pile": ["JC", "3S", "6C"],
+        "hands": {"anna": [None, None, "10C", "2S", "5C", "8D", "2C"]},
         "pass_count": 0,
         "players": ["anna"],
-        "table": [[{"rank": "10", "suit": "diamonds"}]],
+        "table": [["10D"]],
         "yielded": [],
     }
 
 
 def test_defend(game):
-    nine_of_diamonds = {"rank": "9", "suit": "diamonds"}
-    game._table.add_card(card=nine_of_diamonds)
+    nine_of_D = "9D"
+    game._table.add_card(card=nine_of_D)
     game.defend(
-        base_card=nine_of_diamonds,
-        player="anna",
-        card={"rank": "10", "suit": "diamonds"},
+        base_card=nine_of_D, player="anna", card="10D",
     )
     assert game.serialize() == {
-        "draw_pile": [
-            {"rank": "jack", "suit": "clubs"},
-            {"rank": "3", "suit": "spades"},
-            {"rank": "6", "suit": "clubs"},
-        ],
-        "hands": {
-            "anna": [
-                None,
-                None,
-                {"rank": "10", "suit": "clubs"},
-                {"rank": "2", "suit": "spades"},
-                {"rank": "5", "suit": "clubs"},
-                {"rank": "8", "suit": "diamonds"},
-                {"rank": "2", "suit": "clubs"},
-            ]
-        },
+        "draw_pile": ["JC", "3S", "6C"],
+        "hands": {"anna": [None, None, "10C", "2S", "5C", "8D", "2C"]},
         "pass_count": 0,
         "players": ["anna"],
-        "table": [[nine_of_diamonds, {"rank": "10", "suit": "diamonds"}]],
+        "table": [[nine_of_D, "10D"]],
         "yielded": [],
     }
 
@@ -115,38 +57,11 @@ def test_defend(game):
 @pytest.fixture
 def game_3p():
     return Game.deserialize(
-        draw_pile=[
-            {"rank": "7", "suit": "diamonds"},
-            {"rank": "9", "suit": "clubs"},
-            {"rank": "9", "suit": "diamonds"},
-            {"rank": "10", "suit": "clubs"},
-            {"rank": "8", "suit": "diamonds"},
-        ],
+        draw_pile=["7D", "9C", "9D", "10C", "8D"],
         hands={
-            "anna": [
-                {"rank": "9", "suit": "hearts"},
-                {"rank": "3", "suit": "spades"},
-                {"rank": "king", "suit": "hearts"},
-                {"rank": "4", "suit": "clubs"},
-                {"rank": "4", "suit": "hearts"},
-                None,
-            ],
-            "vasyl": [
-                {"rank": "7", "suit": "clubs"},
-                {"rank": "6", "suit": "diamonds"},
-                {"rank": "jack", "suit": "spades"},
-                {"rank": "7", "suit": "hearts"},
-                None,
-                None,
-            ],
-            "igor": [
-                {"rank": "8", "suit": "hearts"},
-                {"rank": "jack", "suit": "diamonds"},
-                {"rank": "king", "suit": "spades"},
-                {"rank": "5", "suit": "hearts"},
-                {"rank": "jack", "suit": "clubs"},
-                None,
-            ],
+            "anna": ["9H", "3S", "KH", "4C", "4H", None],
+            "vasyl": ["7C", "6D", "JS", "7H", None, None],
+            "igor": ["8H", "JD", "KS", "5H", "JC", None],
         },
         pass_count=0,
         players=["anna", "vasyl", "igor"],
@@ -158,32 +73,11 @@ def game_3p():
 def test_draw(game_3p):
     game_3p.draw()
     assert game_3p.serialize() == {
-        "draw_pile": [{"rank": "8", "suit": "diamonds"}],
+        "draw_pile": ["8D"],
         "hands": {
-            "anna": [
-                {"rank": "9", "suit": "hearts"},
-                {"rank": "3", "suit": "spades"},
-                {"rank": "king", "suit": "hearts"},
-                {"rank": "4", "suit": "clubs"},
-                {"rank": "4", "suit": "hearts"},
-                {"rank": "7", "suit": "diamonds"},
-            ],
-            "vasyl": [
-                {"rank": "7", "suit": "clubs"},
-                {"rank": "6", "suit": "diamonds"},
-                {"rank": "jack", "suit": "spades"},
-                {"rank": "7", "suit": "hearts"},
-                {"rank": "9", "suit": "clubs"},
-                {"rank": "9", "suit": "diamonds"},
-            ],
-            "igor": [
-                {"rank": "8", "suit": "hearts"},
-                {"rank": "jack", "suit": "diamonds"},
-                {"rank": "king", "suit": "spades"},
-                {"rank": "5", "suit": "hearts"},
-                {"rank": "jack", "suit": "clubs"},
-                {"rank": "10", "suit": "clubs"},
-            ],
+            "anna": ["9H", "3S", "KH", "4C", "4H", "7D"],
+            "vasyl": ["7C", "6D", "JS", "7H", "9C", "9D"],
+            "igor": ["8H", "JD", "KS", "5H", "JC", "10C"],
         },
         "pass_count": 0,
         "players": ["anna", "vasyl", "igor"],
@@ -196,32 +90,11 @@ def test_draw_with_pass_count(game_3p):
     game_3p._pass_count = 2
     game_3p.draw()
     assert game_3p.serialize() == {
-        "draw_pile": [{"rank": "8", "suit": "diamonds"}],
+        "draw_pile": ["8D"],
         "hands": {
-            "anna": [
-                {"rank": "9", "suit": "hearts"},
-                {"rank": "3", "suit": "spades"},
-                {"rank": "king", "suit": "hearts"},
-                {"rank": "4", "suit": "clubs"},
-                {"rank": "4", "suit": "hearts"},
-                {"rank": "10", "suit": "clubs"},
-            ],
-            "vasyl": [
-                {"rank": "7", "suit": "clubs"},
-                {"rank": "6", "suit": "diamonds"},
-                {"rank": "jack", "suit": "spades"},
-                {"rank": "7", "suit": "hearts"},
-                {"rank": "7", "suit": "diamonds"},
-                {"rank": "9", "suit": "clubs"},
-            ],
-            "igor": [
-                {"rank": "8", "suit": "hearts"},
-                {"rank": "jack", "suit": "diamonds"},
-                {"rank": "king", "suit": "spades"},
-                {"rank": "5", "suit": "hearts"},
-                {"rank": "jack", "suit": "clubs"},
-                {"rank": "9", "suit": "diamonds"},
-            ],
+            "anna": ["9H", "3S", "KH", "4C", "4H", "10C"],
+            "vasyl": ["7C", "6D", "JS", "7H", "7D", "9C"],
+            "igor": ["8H", "JD", "KS", "5H", "JC", "9D"],
         },
         "pass_count": 0,
         "players": ["anna", "vasyl", "igor"],
