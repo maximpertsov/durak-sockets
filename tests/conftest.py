6cdb9573771dd93b2a4e78346164015cccec917f
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-19 22:04:16 -0400

    revert back to snapshottest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,18 @@
+import json
 import os
 import sys
 
+from snapshottest.formatters import BaseFormatter
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(BASE_DIR)
+
+
+def _assert_value_matches_snapshot(self, test, test_value, snapshot_value, formatter):
+    test.assert_equals(
+        json.loads(formatter.normalize(test_value)), json.loads(snapshot_value)
+    )
+
+
+BaseFormatter.assert_value_matches_snapshot = _assert_value_matches_snapshot
