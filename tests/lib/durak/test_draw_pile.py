e85c839e95d7725fe130675c5f4fe03e9d6ccca2
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-28 09:54:46 -0400

    test updated draw pile logic

--- tests/lib/durak/test_draw_pile.py
+++ tests/lib/durak/test_draw_pile.py
@@ -15,6 +15,26 @@ def test_draw_pile_size(get_all_cards_shuffled):
     seed = 0.4
     subject = DrawPile(drawn_cards=[], seed=seed, lowest_rank="2")
     assert subject.size() == 6
+    # TODO: remove
+    assert subject.serialize() == ["10D", "10C", "2S", "5C", "8D", "2C"]
+    get_all_cards_shuffled.assert_called_with(seed)
+
+
+def test_drawn_cards(get_all_cards_shuffled):
+    seed = 0.4
+    subject = DrawPile(drawn_cards=["10D", "10C"], seed=seed, lowest_rank="2")
+    assert subject.size() == 4
+    # TODO: remove
+    assert subject.serialize() == ["2S", "5C", "8D", "2C"]
+    get_all_cards_shuffled.assert_called_with(seed)
+
+
+def test_lowest_rank_is_six(get_all_cards_shuffled):
+    seed = 0.4
+    subject = DrawPile(drawn_cards=[], seed=seed, lowest_rank="6")
+    assert subject.size() == 3
+    # TODO: remove
+    assert subject.serialize() == ["10D", "10C", "8D"]
     get_all_cards_shuffled.assert_called_with(seed)
 
 
@@ -22,5 +42,6 @@ def test_draw_from_pile(get_all_cards_shuffled):
     seed = 0.4
     subject = DrawPile(drawn_cards=[], seed=seed, lowest_rank="2")
     assert subject.draw(count=2) == ["10D", "10C"]
+    # TODO: remove
     assert subject.serialize() == ["2S", "5C", "8D", "2C"]
     get_all_cards_shuffled.assert_called_with(seed)
