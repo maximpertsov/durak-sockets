2f1cc8278968665cd30709bc843689d3a97ea2e9
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-30 10:11:50 -0400

    refactor draw and player tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 import os
 import sys
 
@@ -13,6 +14,17 @@ def _assert_value_matches_snapshot(self, test, test_value, snapshot_value, forma
     test.assert_equals(
         json.loads(formatter.normalize(test_value)), json.loads(snapshot_value)
     )
+
+
+@pytest.fixture
+def get_draw_pile_cards(mocker):
+    def wrapped(cards):
+        return mocker.patch(
+            "lib.durak.draw_pile.get_all_cards_shuffled",
+            mocker.MagicMock(return_value=cards),
+        )
+
+    return wrapped
 
 
 BaseFormatter.assert_value_matches_snapshot = _assert_value_matches_snapshot
--- tests/lib/durak/test_draw_pile.py
+++ tests/lib/durak/test_draw_pile.py
@@ -4,14 +4,6 @@ from lib.durak.draw_pile import DrawPile
 
 
 @pytest.fixture
-def get_all_cards_shuffled(mocker):
-    return mocker.patch(
-        "lib.durak.draw_pile.get_all_cards_shuffled",
-        mocker.MagicMock(return_value=["10D", "10C", "2S", "5C", "8D", "2C"]),
-    )
-
-
-@pytest.fixture
 def default_parameters():
     return {
         "drawn_cards": [],
@@ -20,17 +12,22 @@ def default_parameters():
     }
 
 
-def test_draw_pile_size(default_parameters, get_all_cards_shuffled):
+@pytest.fixture
+def mocked_draw_cards(get_draw_pile_cards):
+    return get_draw_pile_cards(["10D", "10C", "2S", "5C", "8D", "2C"])
+
+
+def test_draw_pile_size(default_parameters, mocked_draw_cards):
     subject = DrawPile(**default_parameters)
     assert subject.serialize() == {
         **default_parameters,
         "drawn_cards": set(),
         "cards_left": 6,
     }
-    get_all_cards_shuffled.assert_called_with(default_parameters["seed"])
+    mocked_draw_cards.assert_called_with(default_parameters["seed"])
 
 
-def test_drawn_cards(default_parameters, get_all_cards_shuffled):
+def test_drawn_cards(default_parameters, mocked_draw_cards):
     args = {**default_parameters, "drawn_cards": ["10D", "10C"]}
     subject = DrawPile(**args)
     assert subject.serialize() == {
@@ -38,10 +35,10 @@ def test_drawn_cards(default_parameters, get_all_cards_shuffled):
         "drawn_cards": set(["10D", "10C"]),
         "cards_left": 4,
     }
-    get_all_cards_shuffled.assert_called_with(default_parameters["seed"])
+    mocked_draw_cards.assert_called_with(default_parameters["seed"])
 
 
-def test_lowest_rank_is_six(default_parameters, get_all_cards_shuffled):
+def test_lowest_rank_is_six(default_parameters, mocked_draw_cards):
     args = {**default_parameters, "lowest_rank": "6"}
     subject = DrawPile(**args)
     assert subject.serialize() == {
@@ -50,10 +47,10 @@ def test_lowest_rank_is_six(default_parameters, get_all_cards_shuffled):
         "cards_left": 3,
         "lowest_rank": "6",
     }
-    get_all_cards_shuffled.assert_called_with(default_parameters["seed"])
+    mocked_draw_cards.assert_called_with(default_parameters["seed"])
 
 
-def test_draw_from_pile(default_parameters, get_all_cards_shuffled):
+def test_draw_from_pile(default_parameters, mocked_draw_cards):
     subject = DrawPile(**default_parameters)
     assert subject.draw(count=2) == ["10D", "10C"]
     assert subject.serialize() == {
@@ -61,4 +58,4 @@ def test_draw_from_pile(default_parameters, get_all_cards_shuffled):
         "drawn_cards": set(["10D", "10C"]),
         "cards_left": 4,
     }
-    get_all_cards_shuffled.assert_called_with(default_parameters["seed"])
+    mocked_draw_cards.assert_called_with(default_parameters["seed"])
