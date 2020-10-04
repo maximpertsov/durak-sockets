04772a4a2a5358315db5727e5a8034311bcdd456
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-10-04 11:55:25 -0400

    update 1 snapshot test

--- tests/lib/durak/test_scenarios.py
+++ tests/lib/durak/test_scenarios.py
@@ -8,36 +8,36 @@ from main import handle_durak_message
 
 SCENARIO_INPUT_FILES = glob.glob(
     os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "scenario_inputs", "*.json"
+        os.path.dirname(os.path.realpath(__file__)), "scenario_inputs/**/input.json"
     )
 )
+SCENARIO_OUTPUT_FILENAME = "output.json"
+
+
+def format_json(json_text):
+    loaded = json.loads(json_text)
+    return json.dumps(loaded, indent=2)
 
 
 @pytest.fixture
-def assert_handle_message(mocker):
-    mocked_persist = mocker.patch("main.persist")
+def assert_snapshot_matches(mocker, snapshot):
+    mocker.patch("main.persist")
 
-    async def wrapped(from_state, to_state):
-        result = await handle_durak_message(from_state)
-        assert json.loads(result) == json.loads(to_state)
-        mocked_persist.assert_called_once()
-
-    return wrapped
-
-
-@pytest.fixture
-def assert_snapshot_matches(assert_handle_message, snapshot):
     async def wrapped(input_path):
         with open(input_path, "r") as f:
-            actual = await handle_durak_message(f.read())
-            snapshot.assert_match(actual)
+            actual = format_json(await handle_durak_message(f.read()))
+
+        snapshot.snapshot_dir = os.path.dirname(input_path)
+        snapshot.assert_match(actual, SCENARIO_OUTPUT_FILENAME)
 
     return wrapped
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "input_path", SCENARIO_INPUT_FILES, ids=lambda p: os.path.split(p)[1]
+    "input_path",
+    SCENARIO_INPUT_FILES,
+    ids=lambda p: os.path.split(os.path.dirname(p))[1],
 )
 async def test_scenarios(input_path, assert_snapshot_matches):
     await assert_snapshot_matches(input_path)
